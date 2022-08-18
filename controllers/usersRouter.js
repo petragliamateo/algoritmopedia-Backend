5e1c155eafb45bae6e967e8740a18680103f332f
Mateo <petragliamateo@gmail.com>
2022-08-18 16:25:23 -0300

    add notification sender

--- controllers/usersRouter.js
+++ controllers/usersRouter.js
@@ -1,30 +1,36 @@
 /* eslint-disable no-case-declarations */
 const usersRouter = require('express').Router();
-let users = require('./usersData.json')
-const fs = require('fs')
+const fs = require('fs');
+let users = require('./usersData.json');
+// Implementar DB. MongoDB?
 
 function addData(user) {
   fs.readFile('./controllers/usersData.json', (err, data) => {
-    if(err) return console.log(err);
-    const datos = JSON.parse(data.toString())
+    if (err) return console.log(err);
+    const datos = JSON.parse(data.toString());
     datos.push(user);
     fs.writeFile('./controllers/usersData.json', JSON.stringify(datos), (e) => {
-      if (err) console.log(err);
-    })
-  })
+      if (e) console.log(e);
+    });
+    return null;
+  });
 }
 
 usersRouter.get('/', (req, res) => {
   res.json(users);
-})
+});
 
 usersRouter.post('/', (req, res) => {
   const { body } = req;
   const date = new Date();
-  const data = { ...body, id: date }
-  addData(data)
+  const data = { ...body, id: date };
+  if (users.map((u) => u.token).includes(body.token)) {
+    res.json('Token alredy exist');
+    return;
+  }
+  addData(data);
   users = users.concat(data);
   res.json(users);
-})
+});
 
 module.exports = usersRouter;
